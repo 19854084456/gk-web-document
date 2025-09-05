1f3d528d860fbffaec28dffc7e8220c7131680b1
19854084456 <19854084456@163.com>
2025-09-05 23:33:26 +0800

    1

--- gk-web-document/docs/.vuepress/config.js
+++ gk-web-document/docs/.vuepress/config.js
@@ -3,13 +3,13 @@ import { defineUserConfig } from 'vuepress'
 import { webpackBundler } from '@vuepress/bundler-webpack'
 
 export default defineUserConfig({
-  lang: 'en-US',
+  lang: 'en-CH',
 
-  title: 'VuePress',
+  title: '',
   description: 'My first VuePress Site',
 
   theme: defaultTheme({
-    logo: 'https://vuejs.press/images/hero.png',
+    logo: 'https://gd-wordpress-main.oss-accelerate.aliyuncs.com/wp-content/uploads/2023/12/%E6%9E%81%E5%AE%A2%E8%B7%B3%E5%8A%A8-1-e1703749280591.png',
 
     navbar: ['/', '/get-started'],
   }),
